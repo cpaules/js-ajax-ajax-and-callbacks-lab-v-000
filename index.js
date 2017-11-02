a407ab4b47307022e82749f27356c3f533a85d7c
Chase Paules <cpaules@gmail.com>
2017-11-02 18:38:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  const input = $("#searchTerms")[0].value;
+  $.get(`https://api.github.com/search/repositories?q=${input}`, data => {
+    $("#results").html(showRepositories(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+function showRepositories(repos) {
+  const repoList = '<ul>' + repos.items.map(r => {
+   return (`
+    <li>
+      <h2><a href="${r.html_url}">${r.name}</a></h2>
+      <p>Watchers: ${r.watchers_count}</p>
+      <p>Forks: ${r.forks_count}</p>
+      <p>Issues: ${r.open_issues_count}</p>
+    </li>`
+    )
+  }).join('') + "</ul>"
+  document.getElementById("results").innerHTML = repoList
+}
